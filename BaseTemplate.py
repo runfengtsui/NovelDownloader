10fdee4dfbe33b4eaad1be838993c852ea7d1dc6
Runfeng Tsui <runfengtsui@outlook.com>
2022-11-28 11:34:23 +0800

    initial commit

---
+++ BaseTemplate.py
@@ -0,0 +1,33 @@
+import requests
+from fake_useragent import UserAgent
+from freeproxy import freeproxy
+
+
+class BaseTemplate:
+
+    def __init__(self, bookname, bookurl) -> None:
+        self.bookname = bookname
+        self.bookurl = bookurl
+        proxy_sources = ['proxylistplus', 'kuaidaili']
+        self.fp_client = freeproxy.FreeProxy(proxy_sources=proxy_sources)
+        # self.proxy = self.fp_client.getrandomproxy()
+        
+    def get_html_text(self, url):
+        try:
+            ua = UserAgent()
+            proxy = self.fp_client.getrandomproxy()
+            r = requests.get(url, headers={'User-Agent': ua.random}, proxies=proxy)
+            r.raise_for_status()
+            r.encoding = r.apparent_encoding
+            return r.text
+        except:
+            print(f'网页：{url}爬取失败!')
+            return ""
+    
+    def save_chapter(self, chapter_name, chapter_content):
+        with open(f"{self.bookname}.txt", 'a', encoding='utf-8') as f:
+            f.write(chapter_name + '\n')
+            for sentence in chapter_content[:-1]:
+                f.write(sentence + '\n')
+            f.write('\n' * 2)
+
---
+++ IBiQiuGe.py
@@ -0,0 +1,39 @@
+from BaseTemplate import BaseTemplate
+# from freeproxy import freeproxy
+from lxml import etree
+import time
+import random
+
+
+class IBiQiuGe(BaseTemplate):
+
+    def parse_book(self):
+        html = self.get_html_text(self.bookurl)
+        while html == "":
+            html = self.get_html_text(self.bookurl)
+        parse_html = etree.HTML(html)
+        title_list = parse_html.xpath('/html/body/div[3]/div[2]/a/text()')
+        url_list = parse_html.xpath('/html/body/div[3]/div[2]/a/@href')
+        return zip(title_list, url_list)
+    
+    
+    def parse_chapters(self, chapter_url):
+        html = self.get_html_text(chapter_url)
+        while html == "":
+            # self.proxy = self.fp_client.getrandomproxy()
+            html = self.get_html_text(chapter_url)
+        parse_html = etree.HTML(html)
+        content = parse_html.xpath('//*[@id="article"]/text()')
+        return content
+    
+    def crawl_booktxt(self):
+        chapters_list = list(self.parse_book())
+        for chapter_name, url in chapters_list:
+            time.sleep(random.random())
+            first_page = "https://www.ibiqiuge.com" + url
+            chapter_content = self.parse_chapters(first_page)
+            second_page = first_page.replace(".html", "_2.html")
+            chapter_content += self.parse_chapters(second_page)
+            self.save_chapter(chapter_name, chapter_content)
+            print(f"{chapter_name} 保存成功!")
+
