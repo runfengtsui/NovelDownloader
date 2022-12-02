8dc34c3e1b797e4f87b35810796d2bed55bdf2db
Runfeng Tsui <runfengtsui@outlook.com>
2022-12-02 21:14:43 +0800

    删除初始化所需书名信息，将crawl函数移至基类，现笔趣阁无法适配

--- noveldl/BaseTemplate.py
+++ noveldl/BaseTemplate.py
@@ -1,19 +1,20 @@
 import requests
 from fake_useragent import UserAgent
 from freeproxy import freeproxy
+import time
+import random
 
 
 class BaseTemplate:
 
-    def __init__(self, bookname, bookurl) -> None:
-        self.bookname = bookname
+    def __init__(self, bookurl: str) -> None:
+        self.bookname = ""
         self.bookurl = bookurl
         proxy_sources = ['proxylistplus', 'kuaidaili']
         self.fp_client = freeproxy.FreeProxy(proxy_sources=proxy_sources)
         self.total_time = 0
-        # self.proxy = self.fp_client.getrandomproxy()
         
-    def get_html_text(self, url):
+    def get_html_text(self, url: str) -> str:
         try:
             ua = UserAgent()
             proxy = self.fp_client.getrandomproxy()
@@ -22,13 +23,21 @@ class BaseTemplate:
             r.encoding = r.apparent_encoding
             return r.text
         except:
-            print(f'网页：{url}爬取失败!')
+            print(f'网页：{url} 爬取失败!')
             return ""
     
-    def save_chapter(self, chapter_name, chapter_content):
+    def save_chapter(self, chapter_name, chapter_content) -> None:
         with open(f"{self.bookname}.txt", 'a', encoding='utf-8') as f:
             f.write(chapter_name + '\n')
-            for sentence in chapter_content[:-1]:
+            for sentence in chapter_content:
                 f.write(sentence + '\n')
             f.write('\n' * 2)
+
+    def crawl(self) -> None:
+        chapters_list = self.parse_book()
+        for chapter_name, url in chapters_list:
+            time.sleep(random.random())
+            chapter_content = self.parse_chapters(url)
+            self.save_chapter(chapter_name, chapter_content)
+            print(f"{chapter_name} 保存成功!")
 
--- noveldl/Chinese81.py
+++ noveldl/Chinese81.py
@@ -1,38 +1,27 @@
 from noveldl.BaseTemplate import BaseTemplate
-# from freeproxy import freeproxy
 from lxml import etree
-import time
-import random
+from typing import List
 
 
 class Chinese81(BaseTemplate):
 
-    def parse_book(self):
+    def parse_book(self) -> List:
         html = self.get_html_text(self.bookurl)
         while html == "":
             html = self.get_html_text(self.bookurl)
         parse_html = etree.HTML(html)
+        self.bookname = parse_html.xpath('//*[@id="info"]/h1/text()')[0]
         title_list = parse_html.xpath('//div[@id="list"]/dl/dd/a/text()')
         url_list = parse_html.xpath('//div[@id="list"]/dl/dd/a/@href')
-        return zip(title_list, url_list)
+        return list(zip(title_list, url_list))
     
     
-    def parse_chapters(self, chapter_url):
-        html = self.get_html_text(chapter_url)
+    def parse_chapters(self, chapter_url: str) -> List:
+        url = "https://www.81zw.com" + chapter_url
+        html = self.get_html_text(url)
         while html == "":
-            # self.proxy = self.fp_client.getrandomproxy()
-            html = self.get_html_text(chapter_url)
+            html = self.get_html_text(url)
         parse_html = etree.HTML(html)
         content = parse_html.xpath('//*[@id="content"]/text()')
-        return content
-    
-    def crawl_81zw(self):
-        start_time = time.time()
-        chapters_list = list(self.parse_book())
-        for chapter_name, url in chapters_list:
-            time.sleep(random.random())
-            chapter_content = self.parse_chapters("https://www.81zw.com" + url)
-            self.save_chapter(chapter_name, chapter_content)
-            print(f"{chapter_name} 保存成功!")
-        self.total_time = time.time() - start_time
+        return content[:-33]
 
